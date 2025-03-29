d5fd9e496ea58a2eeaff95417ffe899596afd38e
Pavel <rainbow343@yandex.ru>
2025-03-29 20:04:00 +0300

    feat: add ci/cd, small fixes

---
+++ bot/utils.py
@@ -0,0 +1,3 @@
+def get_bool_env(value):
+    enabled_values = ("true", "t", "yes", "y", "1")
+    return value.lower() in enabled_values
--- bot/vivobot.py
+++ bot/vivobot.py
@@ -7,13 +7,16 @@ from random import randint
 import dotenv
 import telebot.async_telebot
 
+from bot.utils import get_bool_env
 from bot.vivopack import messages_pack
 
 dotenv.load_dotenv()
 
 API_TOKEN = os.getenv("BOT_TOKEN")
+
 ADMIN_ID = os.getenv("ADMIN_ID", "909313568")
 TARGET_USERS = os.getenv("TARGET_USER_IDS").split(",")
+IS_ALLOW = get_bool_env(os.getenv("IS_ALLOW", "True"))
 
 bot = telebot.async_telebot.AsyncTeleBot(API_TOKEN, "Markdown")
 
@@ -21,9 +24,10 @@ bot = telebot.async_telebot.AsyncTeleBot(API_TOKEN, "Markdown")
 @bot.message_handler(func=lambda message: True)
 async def echo_message(message):
     try:
-        if message.from_user.id in TARGET_USERS:
+        if str(message.from_user.id) in TARGET_USERS and IS_ALLOW:
             await bot.reply_to(
-                message, messages_pack[randint(1, len(messages_pack) - 1)],
+                message,
+                messages_pack[randint(1, len(messages_pack) - 1)],
             )
     except Exception as e:
         print(e)
